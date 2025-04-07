c32fdf544a107377e6ec658bbee93ef355fc6a06
anouar garbaa <g.anouar@yahoo.com>
2025-04-07 22:32:35 +0200

    Setup env Variables

--- src/middlewares/auth/auth.middleware.ts
+++ src/middlewares/auth/auth.middleware.ts
@@ -1,4 +1,6 @@
 import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+//import jwt from 'jsonwebtoken';
+import * as jwt from "jsonwebtoken";
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
@@ -10,13 +12,22 @@ export class AuthMiddleware implements NestMiddleware {
     // *2 invalid token throw a new exception (401)
     if (!token) {
       res.status(HttpStatus.UNAUTHORIZED).json({
-        message: 'Invalid token provided',
+        message: 'Unauthorized',
       });
     }
 
-    // *4 verify the token
+    // *3 verify the token
+    console.log(process.env.JWT_SECRET)
+    //"!" right after process.env.JWT_KEY actually, exclamation (!) says, hey typescript, don't worry, don't check this.
+    //const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+  
+    // if (!decoded) {
+    //   return res.status(HttpStatus.UNAUTHORIZED).json({
+    //     message: 'Invalid token provided',
+    //   });
+    // }
 
-    // *5 valid token continue to the next middleware
+    // *4 valid token continue to the next middleware
 
     next();
   }
