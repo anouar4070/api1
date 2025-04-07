225163990bff53d7b7735c28ad22c58fbe6010d4
anouar garbaa <g.anouar@yahoo.com>
2025-04-08 00:09:29 +0200

    @Roles decorator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import { RolesGuard } from './guards/auth/roles/roles.guard';
 //import dotenv from 'dotenv';
 //dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //app.useGlobalGuards(new RolesGuard())
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
