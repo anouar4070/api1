cbe7e225b0209af948c4b72093bf769881271756
anouar garbaa <g.anouar@yahoo.com>
2025-04-08 10:48:35 +0200

    Custom upperCase Pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DashboardModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DashboardModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
